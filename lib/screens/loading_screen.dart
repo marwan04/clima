cc98f25cf27b1675b2df48caca21d404cdda9d47
marwan04 <marwanalrfai5@gmail.com>
2023-08-13 19:55:02 +0300

    lec 147

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,6 +21,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
     http.Response response =await http.get(Uri.parse('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=98bba56f6756c7a55dce8ed85237fae9'));
     if (response.statusCode == 200) {
       String data = response.body;
+      print(data);
+
+      var decodedData = jsonDecode(data);
+      
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      print(temperature);
     }
     else {
       print(response.hashCode);
